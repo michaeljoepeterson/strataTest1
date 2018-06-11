5ee8053eacb04670f1a587c27f9f13b446e2f64c
Michael Peterson <michaeljoepeterson@gmail.com>
2018-06-11 08:03:38 -0600

    finished refactoring old functions in galleryjs

--- js/gallery.js
+++ js/gallery.js
@@ -1,29 +1,25 @@
-$(document).ready(function() {
 
-	function displayModal(event){
+function displayModal(event){
 
 	const imageNumber = $(this).attr("data-id");
 
 	$("#myModal").css("display", "block");
-	//modal.style.display = "block";
 	$("#img01").attr("src", modalData1[imageNumber].source);
 	$("#captionText1").text(modalData1[imageNumber].contentText)
 
-	}
+}
 
-
-	$('.galleryImage').click(displayModal);
-	$('.carousel-img').click(displayModal);
-	//cick anywhere to close
-	/*
-	modal.addEventListener('click',function(){
-	this.style.display="none";
-	})
-	*/
-	
+function closeModal(){
 	$("#myModal").click(function(event){
 		$(this).css("display","none");
 		//console.log("clicked");
 	});
+}
+	
+function intializePage(){
+	$('.galleryImage').click(displayModal);
+	$('.carousel-img').click(displayModal);
+	closeModal();
+}
 
-});
+$(intializePage);
